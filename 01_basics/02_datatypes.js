2a5f9b4fb00a0f3fda818b29a69f64901def54c9
Kishan Kumar Singh <105405058+jammy25@users.noreply.github.com>
2023-08-16 02:41:55 +0000

    dataTypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3); // we are using nodejs, not browser
+
+
+let name = "Flash";
+let age = 18;
+let isLoggedIn = false;
+let state;
+
+
+// number => range upto 2 to the power 53
+// biginit
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol
+
+// object
+
+console.log(typeof "Flash");
+console.log(typeof age);
+
+console.log(typeof undefined); // o/p => undefined
+console.log(typeof null); // o/p => object
