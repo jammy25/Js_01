8f1e550888a912a8406de2e1d293808ba988e503
Kishan Kumar Singh <105405058+jammy25@users.noreply.github.com>
2023-08-17 04:31:30 +0000

    short summary of dataTypes

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,34 @@
+// Primitive
+
+// 7 Types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let useEmail;
+
+const id = Symbol("123");
+const anotherId = Symbol("123");
+console.log(id === anotherId);
+
+const BigNumber = 46459843165876n;
+
+// Reference (Non Primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "dhruv"]
+
+let myObj = {
+    name: "Kishan",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
