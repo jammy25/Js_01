ee63b99ac46472baa9420f03ce5cf78b99af0c09
Kishan Kumar Singh <105405058+jammy25@users.noreply.github.com>
2023-08-22 12:16:46 +0000

    discussion on numbers in Js

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,14 @@
+const score = 100;
+console.log(score);
+
+const balance = new Number(400);
+console.log(balance);
+
+console.log(balance.toString());
+console.log(balance.toString().length);
+
+const otherNumber = 123.89457;
+console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000;
+console.log(hundreds.toLocaleString('en-In'))
