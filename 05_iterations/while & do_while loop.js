0ee01dbaa22ba694649913ecb02b1c06df4c5dca
Kishan Kumar Singh <105405058+jammy25@users.noreply.github.com>
2023-08-30 04:17:54 +0000

    discussion on while and do while loops

---
+++ 05_iterations/while & do_while loop.js
@@ -0,0 +1,26 @@
+// while
+
+let index = 0
+while (index <= 10) {
+    // console.log(`value of index is ${index}`);
+    index = index + 2
+}
+
+
+let myArr = ['thor, blackWidow, antMan']
+
+let arr = 0
+while (arr < myArr.length) {
+    // console.log(myArr[arr]);
+    arr++
+}
+
+
+// do while loop
+
+// let score = 1
+let score = 11
+do {
+    console.log(`Value of score is ${score}`);
+    score++
+} while (score <= 10);
