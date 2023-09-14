dd38427f290567697b7a4aaa53e2ea4715ca9ed3
Kishan Kumar Singh <105405058+jammy25@users.noreply.github.com>
2023-09-14 17:28:03 +0000

    discussion on prototype, object and new keyword

---
+++ 10_classes_oop/03_object.js
@@ -0,0 +1,12 @@
+// Object
+
+function multipleBy5(num){
+    return num*5
+}
+
+multipleBy5.power = 2;
+
+console.log(multipleBy5(5));
+console.log(multipleBy5.power);
+console.log(multipleBy5.prototype);
+
